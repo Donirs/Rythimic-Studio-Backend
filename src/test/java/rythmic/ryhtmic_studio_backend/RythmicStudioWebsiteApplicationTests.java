0e0fe81dc713052a5ba32b331a2dc018a26a50eb
Donirs <rafhiromadhoni07@gmail.com>
2024-12-02 19:18:10 +0700

    add database

--- src/test/java/rythmic/ryhtmic_studio_backend/RythmicStudioWebsiteApplicationTests.java
+++ src/test/java/rythmic/ryhtmic_studio_backend/RythmicStudioWebsiteApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FitSpaceSportsVenueBookingWebsiteBackendApplicationTests {
+class RythmicStudioWebsiteApplicationTests {
 
 	@Test
 	void contextLoads() {
